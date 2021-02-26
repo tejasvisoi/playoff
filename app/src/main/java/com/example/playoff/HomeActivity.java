5100b1fa6f517f6c6eca94de7e05e866f3f07117
Tejasvi <soi.tejasvi@gmail.com>
2021-02-26 23:47:16 +0530

    first commit

---
+++ app/src/main/java/com/example/playoff/HomeActivity.java
@@ -0,0 +1,64 @@
+package com.example.playoff;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+    EditText et1;
+    ImageView im1;
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        et1=findViewById(R.id.idheadTV);
+        im1=findViewById(R.id.image1);
+        t1=findViewById(R.id.email);
+        // button for logout and initialing our button.
+        Button next = findViewById(R.id.next);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photoUrl = user.getPhotoUrl().toString();
+            Glide.with(this).load(photoUrl).into(im1);
+            et1.setText(name);
+            t1.setText(email);
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+            if(emailVerified==true){
+
+            }
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
+        // adding onclick listener for our logout button.
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/playoff/skills.java
@@ -0,0 +1,14 @@
+package com.example.playoff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class skills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skills);
+    }
+}
